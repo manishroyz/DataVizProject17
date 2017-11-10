138514f564c90a7a7319079f62f6e60ba877188d
Manish Roy <manish.roy@utah.edu>
2017-11-10 13:30:10 -0700

    Data filtering after brush selection complete

--- website/public/js/selectionChart.js
+++ website/public/js/selectionChart.js
@@ -17,6 +17,51 @@ class SelectionChart {
         console.log(selectedTime);
 
         //Update the visualization on brush events over the Circle chart
+        d3.csv("data/BostonMA.csv", function (error, bikeData) {
+            console.log(bikeData);
+
+            let selectedMonths = selectedTime['selectedMonths'];
+            let selectedDays = selectedTime['selectedDays'];
+            let selectedHours = selectedTime['selectedHours'];
+
+            //console.log(selectedMonths);
+            //console.log(selectedDays);
+            //console.log(selectedHours);
+
+            let startMonth =selectedMonths[0];
+            let endMonth = selectedMonths[(selectedMonths.length)-1];
+            let startDay =selectedDays[0];
+            let endDay = selectedDays[(selectedDays.length)-1];
+            let startHour = selectedHours[0];
+            let endHour = selectedHours[(selectedHours.length)-2];
+
+            //console.log(startMonth);
+            //console.log(endMonth);
+            //console.log(startDay);
+            //console.log(endDay);
+            //console.log(startHour);
+            //console.log(endHour);
+
+            let parser = d3.timeParse("%m/%d/%Y %H:%M");
+
+            // FILTERING DATA BASED ON DATE RANGE
+            let bikeDataFiltered =[];
+            bikeData.forEach(function (d) {
+                let parsedDate = parser(d['starttime']);
+                //console.log(parsedDate);
+                let month = parsedDate.getMonth() + 1;  //0-11
+                let day = parsedDate.getDate();  //1-31
+                let hour = parsedDate.getHours(); //0-23
+                //console.log(month);
+                //console.log(day);
+                //console.log(hour);
+
+                if ((month >= startMonth && month <=endMonth) && (day >= startDay && day <= endDay) && (hour >= startHour && hour < endHour)) {
+                    bikeDataFiltered.push(d);
+                }
+            });
+            console.log(bikeDataFiltered);
+        });
 
     };
 
